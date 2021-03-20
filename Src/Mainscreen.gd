a97a8b999826ff91fc45789559790b62440ba08c
Mihin DIssanayake <73010893+MihinMUD@users.noreply.github.com>
2021-03-20 06:49:12 +0530

    New verion 0.1
    added new upadted files

--- Src/Mainscreen.gd
+++ Src/Mainscreen.gd
@@ -2,7 +2,11 @@ extends Control
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene("res://Level.tscn")
+		get_tree().change_scene("res://Levels/Level1.tscn")
 
 func _on_Button_pressed() -> void:
-	get_tree().change_scene("res://Level.tscn")
+	get_tree().change_scene("res://Levels/Level1.tscn")
+
+
+func _on_About_pressed() -> void:
+	get_tree().change_scene("res://Credits.tscn")
